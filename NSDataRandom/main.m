d133d64c8a0332af160cede900422a7e26d38181
cstaylor <github@cstaylor.org>
2013-06-29 12:33:24 +0900

    Initial Commit

---
+++ NSDataRandom/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NSDataRandom
+//
+//  Created by Christopher Taylor on 6/29/13.
+//  Copyright (c) 2013 Christopher Taylor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
